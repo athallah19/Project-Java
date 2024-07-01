5a1bd9dc23a83a9edaa8c29ce09e1ae90110b0bf
Athallah Abhi <127083737+athallah19@users.noreply.github.com>
2024-07-01 14:27:55 +0700

    Add files via upload

---
+++ formpegawai/src/formpegawai/tgsformpegawai.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formpegawai;
+
+/**
+ *
+ * @author PC05
+ */
+public class tgsformpegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
